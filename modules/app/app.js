c71db0746b88f0de8dc52fa2d14c0d6e6573e178
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-05-31 12:27:25 -0700

    deploy

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const { CLIENT_ORIGIN } = require('../../config');
 
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express()
 
 app.use(cors({
-    origin: CLIENT_ORIGIN
+  origin: CLIENT_ORIGIN
 }))
 
 app.use('/', require('../people/people.router'));
